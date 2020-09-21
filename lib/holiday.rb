efea05dd272739b76cd6aae47e7f1a59e6580580
Aiden Marques <aiden.marques94@gmail.com>
2020-09-21 20:54:36 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -18,30 +18,34 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
+  
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
+  
+ holiday_hash[:winter][:christmas] << supply
+  holiday_hash[:winter][:new_years] << supply
+
 
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
-  # again, holiday_hash is the same as the ones above
-  # add the second argument to the memorial day array
-
+  holiday_hash[:spring][:memorial_day] << supply
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  # code here
-  # remember to return the updated hash
-
+  
+  holiday_hash[season][holiday_name] = supply_array
+  
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -52,19 +56,28 @@ def all_supplies_in_holidays(holiday_hash)
   #   New Years: Party Hats
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
-  # etc.
-
+  # etc
+  holiday_hash.each do |season, holiday|
+    puts "#{season.to_s.capitalize!}:"
+    holiday.each do |occasion, supplies|
+      separated_words = occasion.to_s.split("_")
+      capitalized_words = separated_words.map do |word|
+        word.capitalize!
+      end
+      capitalized_occasion = capitalized_words.join(" ")
+      puts "  #{capitalized_occasion}: #{supplies.join(", ")}"
+    end
+  end
 end
 
-def all_holidays_with_bbq(holiday_hash)
-  # return an array of holiday names (as symbols) where supply lists
-  # include the string "BBQ"
+  def all_holidays_with_bbq(holiday_hash)
+    bbq_holidays = holiday_hash.map do |seasons, event|
+      event.map do |occasions, components|
+        if components.include?("BBQ")
+          occasions
+        end
+      end
+    end
+  bbq_holidays.flatten.compact
 
-end
-
-
-
-
-
-
-
+  end
